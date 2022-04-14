63d32187322c80f85232597463bc24adf0d8dd11
m83city <rostorguievv@gmail.com>
2022-04-14 15:41:16 +0300

    Testing hook useContext with Drawer(antd)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { But } from './component/But';
 import Main  from "../src/app/Main"
- import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
+import { Instrument } from './component/Instrument';
+import { Drawer, Button } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
         <Main></Main>
-        <But></But>
+        <Drawer visible={true} placement= "right" > 
+          <Instrument></Instrument>
+        </Drawer>
+        <Instrument/>
     </div>
   );
 }
---
+++ src/component/Instrument.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Button, Input, Dropdown} from "antd"
+import 'antd/dist/antd.css'
+
+export const Instrument = () =>{
+
+    return(
+        <div>
+            <Button> Button one</Button>
+            <Button>Button two</Button>
+            <Input></Input>
+            
+        </div>
+    
+    )
+
+}
